d00c149869b34e737c1eee86b03b1f5242391568
Raphael Rapozo Rebello de Rezende <123587313+raphaelrrrezende@users.noreply.github.com>
2023-04-27 00:41:42 -0300

    Iterando em listas

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,8 +1,35 @@
-// Som Pom ------------------------------------------------
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+/*
+let contador = 0;
+
+while(contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    // template string
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+}
+*/
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; // template string
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    }
+
+}
