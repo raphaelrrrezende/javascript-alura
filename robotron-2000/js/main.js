bf69de5b3467e308c2d330f2725f29f9cc045c61
Raphael Rapozo Rebello de Rezende <123587313+raphaelrrrezende@users.noreply.github.com>
2023-05-10 23:03:06 -0300

    Montando uma peça

--- robotron-2000/js/main.js
+++ robotron-2000/js/main.js
@@ -1,12 +1,19 @@
-const robotron = document.querySelector('#robotron');
+const subtrair = document.querySelector('#subtrair');
+const somar = document.querySelector('#somar');
+const braco = document.querySelector('#braco');
 
-robotron.addEventListener('click', (evento) => {
-    console.log(evento);
+const controle = document.querySelectorAll('.controle-ajuste');
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.textContent);
+    });
 });
 
-function dizOla(nome) {
-    console.log('Olá, ' + nome);
-    console.log('Bem-vindo(a) ao Robotron 2000!');
+function manipulaDados(operacao) {
+    if(operacao === '-') {
+        braco.value = parseInt(braco.value) - 1;
+    } else {
+        braco.value = parseInt(braco.value) + 1;
+    }
 }
-
-dizOla('Raphael');
