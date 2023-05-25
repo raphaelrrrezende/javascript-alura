701b4f6ee0f614a44bda30e94d5c2a520519b6ca
Raphael Rapozo Rebello de Rezende <123587313+raphaelrrrezende@users.noreply.github.com>
2023-05-25 09:12:30 -0300

    Armazenando dados na WEB

--- mochila-de-viagem/js/main.js
+++ mochila-de-viagem/js/main.js
@@ -1,10 +1,18 @@
 const form = document.getElementById('novoItem');
 const lista = document.getElementById('lista');
+const itens = [];
 
 form.addEventListener('submit', (evento) => {
-    evento.preventDefault()
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value);
+    evento.preventDefault();
+
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade'];
+
+    criaElemento(nome.value, quantidade.value);
+
+    nome.value = '';
+    quantidade.value = '';
 });
 
 function criaElemento(nome, quantidade) {
@@ -19,4 +27,13 @@ function criaElemento(nome, quantidade) {
     novoItem.innerHTML += nome;
     
     lista.appendChild(novoItem);
+
+    const itemAtual = {
+        'nome': nome,
+        'quantidade': quantidade
+    }
+
+    itens.push(itemAtual);
+
+    localStorage.setItem('item', JSON.stringify(itens));
 };
