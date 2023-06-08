783a69b459f8c24091787163c57f62d97006f39b
Raphael Rapozo Rebello de Rezende <123587313+raphaelrrrezende@users.noreply.github.com>
2023-06-08 14:25:46 -0300

    Entendendo o JavaScript

--- consumindo-dados-api/script.js
+++ consumindo-dados-api/script.js
@@ -1,1 +1,13 @@
-console.log('Hello, World!');
+console.log('Mandando "oi" para o amigo');
+
+function mandaMensagem() {
+
+    console.log('Tudo bem?');
+    console.log('Vou te mandar uma solicitação');
+    console.log('Solicitação recebida!');
+
+};
+
+setTimeout(mandaMensagem, 5000);
+
+console.log('Vlw!');
