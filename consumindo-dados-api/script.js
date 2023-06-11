bbc61e19dede39bfd31452d09475170443b0c6ae
Raphael Rapozo Rebello de Rezende <123587313+raphaelrrrezende@users.noreply.github.com>
2023-06-11 20:33:24 -0300

    Criando funções assíncronas

--- consumindo-dados-api/script.js
+++ consumindo-dados-api/script.js
@@ -1,12 +1,21 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json())
-    .then(r => {
-        if(r.erro) {
-            throw Error('Esse CEP não existe!')
-        } else
-        console.log(r)
-    })
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('Processamento concluído!'));
+async function buscaEndereco(cep) {
 
-console.log(consultaCEP);
+    try {
+
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+
+        if(consultaCEPConvertida.erro) {
+            throw Error('CEP inexistente!');
+        }
+
+        console.log(consultaCEPConvertida);
+
+        return consultaCEPConvertida;
+        
+    } catch (erro) {
+        console.log(erro);
+    }
+    
+};
+
