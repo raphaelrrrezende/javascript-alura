0fe840777c33ea640bd2b83e8e2a1cee9b37da37
Raphael Rapozo Rebello de Rezende <123587313+raphaelrrrezende@users.noreply.github.com>
2023-06-12 14:25:52 -0300

    Manipulando formulário

--- consumindo-dados-api/script.js
+++ consumindo-dados-api/script.js
@@ -1,5 +1,9 @@
 async function buscaEndereco(cep) {
 
+    var mensagemErro = document.getElementById('erro');
+
+    mensagemErro.innerHTML = '';
+
     try {
 
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
@@ -9,13 +13,31 @@ async function buscaEndereco(cep) {
             throw Error('CEP inexistente!');
         }
 
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+        var complemento = document.getElementById('complemento');
+        var bairro = document.getElementById('bairro');
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+        complemento.value = consultaCEPConvertida.complemento;
+        bairro.value = consultaCEPConvertida.bairro;
+
         console.log(consultaCEPConvertida);
 
         return consultaCEPConvertida;
         
     } catch (erro) {
+
+        mensagemErro.innerHTML = `<p>CEP inválido! Tente novamente.</p>`;
         console.log(erro);
+
     }
     
 };
 
+var cep = document.getElementById('cep');
+
+cep.addEventListener('focusout', () => buscaEndereco(cep.value));
