6f08c5127f11a4740541ba1b43d77d26a3585afd
Raphael Rapozo Rebello de Rezende <123587313+raphaelrrrezende@users.noreply.github.com>
2023-06-14 09:33:28 -0300

    Filter: aplicando diferentes filtros

--- alura-books/app/metodoForEach.js
+++ alura-books/app/metodoForEach.js
@@ -2,23 +2,25 @@ const inserirLivros = document.getElementById('livros');
 
 function exibirLivros(listaDeLivros) {
 
-    listaDeLivros.forEach(livro => {
+  inserirLivros.innerHTML = '';
 
-        inserirLivros.innerHTML += `
-        <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}"
-          alt="${livro.alt}" />
-        <h2 class="livro__titulo">
-          ${livro.titulo}
-        </h2>
-        <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
-        <div class="tags">
-          <span class="tag">${livro.categoria}</span>
-        </div>
-        </div>
-        `
+  listaDeLivros.forEach(livro => {
+
+      inserirLivros.innerHTML += `
+      <div class="livro">
+      <img class="livro__imagens" src="${livro.imagem}"
+        alt="${livro.alt}" />
+      <h2 class="livro__titulo">
+        ${livro.titulo}
+      </h2>
+      <p class="livro__descricao">${livro.autor}</p>
+      <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+      <div class="tags">
+        <span class="tag">${livro.categoria}</span>
+      </div>
+      </div>
+      `
         
-    });
+  });
 
 };
