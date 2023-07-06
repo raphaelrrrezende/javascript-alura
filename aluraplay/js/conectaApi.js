9ea97d25139b9863404035906ce389d7bae8dd36
Raphael Rapozo Rebello de Rezende <123587313+raphaelrrrezende@users.noreply.github.com>
2023-07-06 15:21:35 -0300

    Buscando elementos

--- aluraplay/js/conectaApi.js
+++ aluraplay/js/conectaApi.js
@@ -29,9 +29,19 @@ async function criaVideo(titulo, descricao, url, imagem) {
 
 };
 
+async function buscaVideo(termoDeBusca) {
+
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
+    const conexaoConvertida = conexao.json();
+
+    return conexaoConvertida;
+
+};
+
 export const conectaApi = {
 
     listaVideos,
-    criaVideo
+    criaVideo,
+    buscaVideo
     
 };
