8a1582f0237e076bb26db26629cb0d9b9effa3d5
Raphael Rapozo Rebello de Rezende <123587313+raphaelrrrezende@users.noreply.github.com>
2023-07-06 14:49:21 -0300

    Criando novos elementos

--- aluraplay/js/conectaApi.js
+++ aluraplay/js/conectaApi.js
@@ -7,6 +7,31 @@ async function listaVideos() {
 
 };
 
+async function criaVideo(titulo, descricao, url, imagem) {
+
+    const conexao = await fetch('http://localhost:3000/videos', {
+
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil vizualizações`,
+            url: url,
+            imagem: imagem
+        })
+
+    });
+
+    const conexaoConvertida = await conexao.json();
+    return conexaoConvertida;
+
+};
+
 export const conectaApi = {
-    listaVideos
+
+    listaVideos,
+    criaVideo
+    
 };
